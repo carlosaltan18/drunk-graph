49d1604fb4d06c31c81270d57b417b849c813b8a
bcastillo-2022474 <bcastillo-2022474@kinal.edu.gt>
2026-05-03 21:44:51 -0600

    Sobre esta roca edificaré mi iglesia Mateo 16:18

---
+++ src/main/java/com/uvg/drunkgraph/DrunkgraphApplication.java
@@ -0,0 +1,13 @@
+package com.uvg.drunkgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrunkgraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrunkgraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uvg/drunkgraph/DrunkgraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvg.drunkgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrunkgraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
