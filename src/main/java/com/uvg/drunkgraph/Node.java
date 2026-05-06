8eb16677858d094373997f2bf28e0432ea705db0
eldavz <danricar.9@gmail.com>
2026-05-05 23:05:22 -0600

    Feat: Implemented constructor in Node class

--- src/main/java/com/uvg/drunkgraph/Node.java
+++ src/main/java/com/uvg/drunkgraph/Node.java
@@ -3,4 +3,9 @@ package com.uvg.drunkgraph;
 public class Node<T> {
     private T data;
     private Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
 }
